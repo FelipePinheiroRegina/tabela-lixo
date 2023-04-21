b62f5a62cb76dd2a1fcb8eca712ca377636a07eb
Felipe Pinheiro <flppinheiro@hotmail.com>
2023-04-21 13:59:13 -0300

    mexendo no projeto

---
+++ tabela.js
@@ -0,0 +1,8 @@
+let data = window.document.getElementById('data')
+let dataatual = new Date();
+function formatardata(dataatual){
+    let newDate = new Date(dataatual);
+    return ` ${newDate.getDate()}/0${newDate.getMonth() + 1}/${newDate.getFullYear()}`;
+}
+data.innerHTML = formatardata(dataatual)
+data.style.marginLeft = '160px'
