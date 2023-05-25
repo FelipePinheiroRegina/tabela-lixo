f812f9c755afcdd27e26365c926156b4ce741f7c
Felipe Pinheiro <flppinheiro@hotmail.com>
2023-05-24 22:46:09 -0300

    Lixo Atualizado

--- tabela.js
+++ tabela.js
@@ -1,3 +1,26 @@
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+const relogio = setInterval(function time(){
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let seg = dateToday.getSeconds();
+    
+    if (hr < 10) hr = '0' + hr;
+    
+    if (min < 10) min = '0' + min;
+    
+    if (seg < 10) seg = '0' + seg;
+    
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = seg;
+
+    
+})
+
 let data = window.document.getElementById('data')
 let dataatual = new Date();
 function formatardata(dataatual){
@@ -5,7 +28,7 @@ function formatardata(dataatual){
     return ` ${newDate.getDate()}/0${newDate.getMonth() + 1}/${newDate.getFullYear()}`;
 }
 data.innerHTML = formatardata(dataatual)
-data.style.marginLeft = '160px'
+
 
 
 
